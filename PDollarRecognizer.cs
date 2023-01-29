3b9718cea652844f66175ee69d9c55f9cd419c04
asdhx <60109993+asdhx@users.noreply.github.com>
2023-01-29 02:29:36 +0100

    Add files via upload

---
+++ PDollarRecognizer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using PDollarGestureRecognizer;
+
+public class PDollarRecognizer : Singleton<PDollarRecognizer>
+{
+    public PDollarRecognizer()
+    {
+
+    }
+
+    private List<Gesture> trainingSet = new List<Gesture>();
+
+    void Start()
+    {
+        //Set initial values for render input
+
+
+        //Load pre-made gestures
+        TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GameSet/");
+        foreach (TextAsset gestureXml in gesturesXml)
+            trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
+
+        //Load user custom gestures
+        //string[] filePaths = Directory.GetFiles("C:\\Users\\asd\\New Unity Project (3)\\Assets\\PDollar\\Resources\\GameSet", " *.xml");
+        //foreach (string filePath in filePaths)
+        //   trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
+        //trainingSet.Add(GestureIO.ReadGestureFromFile("C:\\Users\\asd\\New Unity Project (3)\\lightning.xml"));
+    }
+
+    public String CallClassifier(List<Point> points)
+    {
+        Gesture candidate = new Gesture(points.ToArray());
+        Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
+        Debug.Log(gestureResult.Score);
+        Debug.Log(gestureResult.GestureClass);
+        return gestureResult.GestureClass; 
+    }
+
+}
---
+++ PDollarWriter.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using PDollarGestureRecognizer;
+
+public class PDollarWriter : MonoBehaviour
+{
+
+
+    private List<Gesture> trainingSet = new List<Gesture>();
+
+    private List<Point> points;
+    private int strokeId;
+
+    private Vector3 virtualKeyPosition;
+    private Rect drawArea;
+
+    private RuntimePlatform platform;
+
+
+    private int deltaFrames;
+
+    public string newGestureName;
+
+
+    void Start()
+    {
+        //Set initial values for render input
+        deltaFrames = 0;
+        strokeId = -1;
+        points = new List<Point>();
+
+
+
+        drawArea = new Rect(0, 0, Screen.width, Screen.height);
+        virtualKeyPosition = Vector2.zero;
+
+        platform = Application.platform;
+
+        //Load pre-made gestures
+    }
+
+    void Update()
+    {
+        //check wheter we are on phone or desktop and notice cursor position
+        if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+        {
+            if (Input.touchCount > 0)
+            {
+                virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+            }
+        }
+        else
+        {
+            if (drawArea.Contains(virtualKeyPosition))
+            {
+
+
+                if (Input.GetMouseButtonDown(0) && deltaFrames == 0)
+                {
+                    strokeId++;
+                    deltaFrames++;
+                }
+                if (Input.GetMouseButton(0) && strokeId > -1)
+                {
+                    virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+                    updatePoints();
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    deltaFrames = 0;
+
+                }
+                if (Input.GetKeyDown("w"))
+                    addGesture();
+
+            }
+        }
+
+    }
+
+
+    void cleanUp()
+    {
+        points.Clear();
+        strokeId = -1;
+    }
+
+
+    void updatePoints()
+    {
+        points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+    }
+
+    void addGesture()
+    {
+        //string fileName = String.Format("{0}/{1}-{2}.xml", Application.persistentDataPath, newGestureName, DateTime.Now.ToFileTime());
+        string fileName = String.Format("{0}.xml", newGestureName);
+        GestureIO.WriteGesture(points.ToArray(), newGestureName, fileName);
+        //trainingSet.Add(new Gesture(points.ToArray(), newGestureName));
+        newGestureName = "default";
+        cleanUp();
+    }
+
+}
